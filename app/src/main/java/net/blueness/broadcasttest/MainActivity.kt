6dd8a43835f3f409eae30f705c79a6058fe7d0b4
Blueness <w0330t@gmail.com>
2017-08-27 10:56:32 +0800

    Initial commit

---
+++ app/src/main/java/net/blueness/broadcasttest/MainActivity.kt
@@ -0,0 +1,48 @@
+package net.blueness.broadcasttest
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private var intentFilter: IntentFilter? = null
+    private var networkChangeReceiver: NetworkChangeReceiver? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        intentFilter = IntentFilter()
+        intentFilter?.addAction("android.net.conn.CONNECTIVITY_CHANGE")
+        networkChangeReceiver = NetworkChangeReceiver()
+        registerReceiver(networkChangeReceiver, intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(networkChangeReceiver)
+    }
+
+    internal inner class NetworkChangeReceiver: BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val connectionManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val networkInfo = connectionManager.activeNetworkInfo
+            if (networkInfo != null && networkInfo.isAvailable) {
+                Toast.makeText(context, "network is available",
+                        Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "network is unavailable",
+                        Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
+
